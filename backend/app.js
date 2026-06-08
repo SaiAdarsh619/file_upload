0c8de2d26ae806aa90e1e01520471435e3a39aa4
SaiAdarsh <adarshsai619@gmail.com>
2026-06-08 15:01:37 +0530

    session length 1 day

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        maxAge: 24 * 60 * 60 * 1000, // 1 days
         httpOnly: true
     }
 }));
